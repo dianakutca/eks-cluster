074be98d267089bd257c65ee45e90e319a0f7a10
dianakutca <dianakutca14@gmail.com>
2023-09-27 20:36:24 +0000

    added output

---
+++ modules/eks/output.tf
@@ -0,0 +1,19 @@
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.cluster.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = aws_security_group.eks_cluster_sg.id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = var.cluster-name
+}
